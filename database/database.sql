21fc37c8ca532d95b62274c23c7922173dcaff55
Hichamelhmaydi <ct1594065@gmail.com>
2024-12-31 12:13:25 +0100

    ajouter les tableaux dans la base de données

---
+++ database/database.sql
@@ -0,0 +1,31 @@
+USE CulturesPartagees;
+
+CREATE TABLE user (
+    id_user INT AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+    nom VARCHAR(20) NOT NULL UNIQUE,
+    prenom VARCHAR(20) NOT NULL, 
+    email VARCHAR (50) NOT NULL UNIQUE, 
+    user_password VARCHAR(20),
+    role ENUM ('utilisateur','auteur')
+);
+CREATE TABLE admin (
+    email VARCHAR(50),
+    admin_password VARCHAR(20) 
+);
+INSERT INTO admin (email, admin_password) VALUES ('admin@gmail.com', 'ABC12332100');
+
+CREATE TABLE categories (
+    id_catr  INT AUTO_INCREMENT PRIMARY KEY ,
+     nom_ca VARCHAR(20) UNIQUE
+);
+
+CREATE TABLE article (
+   id_art  INT AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+   titre VARCHAR(20) NOT NULL ,
+   contenu VARCHAR(10000),
+   statut ENUM ('en attente','accepter','refuser') DEFAULT 'en attente',
+   auteur VARCHAR(20),
+   categorie VARCHAR(20),
+   Foreign Key (auteur) REFERENCES user (nom),
+   Foreign Key (categorie) REFERENCES categories(nom_ca)
+);
